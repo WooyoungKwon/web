03eda0ab43d3a4c5726a0036d893d477c1348e74
Your Name <you@example.com>
2023-11-25 15:21:00 +0900

    20231125

--- routes/posts.js
+++ routes/posts.js
@@ -12,7 +12,13 @@ router.get('/insert', function(req, res, next){
 
 router.get('/read', function(req, res, next){
   const id=req.query.id;
-  res.render('index', {title: '게시글 정보', pageName:'posts/read.ejs', id:id});
+  res.render('index', {title: '게시글정보', pageName:'posts/read.ejs', id:id});
+});
+
+// 'posts/update.ejs'으로 title과 id를 보내줌
+router.get('/update', function(req, res, next){
+  const id=req.query.id;
+  res.render('index', {title: '게시글수정', pageName:'posts/update.ejs', id:id});
 });
 
 
